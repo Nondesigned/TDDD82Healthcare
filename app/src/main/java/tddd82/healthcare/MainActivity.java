358930fac9d800357d202149247376e12b0e43ae
Per Gustavsson <perkan95@gmail.com>
2017-02-17 11:02:51 +0100

    Fungerande avläsning av ID

--- app/src/main/java/tddd82/healthcare/MainActivity.java
+++ app/src/main/java/tddd82/healthcare/MainActivity.java
@@ -1,5 +1,7 @@
 package tddd82.healthcare;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
@@ -8,21 +10,35 @@ import android.nfc.tech.Ndef;
 import android.nfc.tech.NfcA;
 import android.nfc.tech.TagTechnology;
 import android.os.Parcelable;
+import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
 
 import java.nio.ByteBuffer;
 
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "AntonsLog";
+
+    private final static int MY_PERMISSIONS_REQUEST = 1;
+
+    String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.NFC};
+
     EditText usernameInput;
     EditText passwordInput;
+    TextView testinfo;
+    Button nfcbutton;
+
 
     NfcActivity nfcActivity = null;
 
@@ -31,19 +47,79 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usernameInput = (EditText)findViewById(R.id.cardIDInput);
-        passwordInput = (EditText)findViewById(R.id.passwordInput);
+        testinfo = (TextView)findViewById(R.id.testinfo);
+        nfcbutton = (Button)findViewById(R.id.nfcbutton);
+        nfcActivity = new NfcActivity(this);
 
         NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
+        if (mNfcAdapter != null) {
+            Toast.makeText(this, "Fuck you", Toast.LENGTH_SHORT).show();
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.NFC)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.NFC},MY_PERMISSIONS_REQUEST);
+
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
+    @Override
     protected void onNewIntent(Intent intent){
+        Toast.makeText(this, "Tessst", Toast.LENGTH_SHORT).show();
         super.onNewIntent(intent);
         readTextFromNFCTag(intent);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(nfcActivity != null){
+            nfcActivity.enableForeground();
+
+            if (!nfcActivity.getNfc().isEnabled())
+            {
+                Toast.makeText(getApplicationContext(), "Aktivera NFC och tryck på tillbaka.", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));
+            }
+        }
+
+    }
+
+
+
+
+
+
     public void readTextFromNFCTag(Intent intent){
         super.onNewIntent(intent);
+        Vibrator v= (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        v.vibrate(30);
 
         if (intent!= null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())){
             Parcelable[] rawMessages =
@@ -60,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         }
         Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         String rfid = bin2int(tag.getId());
-        usernameInput.setText(rfid);
+        testinfo.setText(rfid);
     }
 
     static String bin2int(byte[] data) {
--- app/src/main/java/tddd82/healthcare/NfcActivity.java
+++ app/src/main/java/tddd82/healthcare/NfcActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.NfcA;
+import android.util.Log;
 
 /**
  * Created by pergu on 16-Feb-17.
@@ -25,6 +26,7 @@ public class NfcActivity {
         this.activity = activity;
         nfc = NfcAdapter.getDefaultAdapter(activity.getApplicationContext());
 
+
         intent = PendingIntent.getActivity(activity,0, new Intent(activity, activity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
 
         IntentFilter tag = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
@@ -41,6 +43,18 @@ public class NfcActivity {
         techListArray = new String[][] {new String[]{NfcA.class.getName()}};
     }
 
+    public void enableForeground()
+    {
+        Log.d("Demo","Foregorund enabled");
+        nfc.enableForegroundDispatch(activity, intent, intentFilterArray, techListArray);
+    }
+
+    public void disableForeground()
+    {
+        Log.d("Demo","Foregorund disabled");
+
+        nfc.disableForegroundDispatch(activity);
+    }
     public NfcAdapter getNfc(){
         return nfc;
     }
