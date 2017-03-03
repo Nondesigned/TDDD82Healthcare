fd0b7746cfb9e37dbb15f175051c4c7409748c5c
Clynch7 <anton.silfver@gmail.com>
2017-03-03 13:11:47 +0100

    stuff

--- app/src/main/java/tddd82/healthcare/MainActivity.java
+++ app/src/main/java/tddd82/healthcare/MainActivity.java
@@ -14,9 +14,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "AntonsLog";
     private String token = null;
     private int requestCode = 1;
-    EditText usernameInput;
-    EditText passwordInput;
-    //TODO ändra layout i main & Göra något med token som returneras i onActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,40 +24,5 @@ public class MainActivity extends AppCompatActivity {
             startActivity(startLoginActivity);
         }
 
-
-        usernameInput = (EditText)findViewById(R.id.cardIDInput);
-        passwordInput = (EditText)findViewById(R.id.passwordInput);
-
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == this.requestCode) {
-            if (resultCode == RESULT_OK) {
-                //Use Data to get string
-                token = data.getStringExtra("RESULT_STRING");
-                Toast toast = Toast.makeText(getApplicationContext(), "test" + this.token, Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        }
-    }
-
-    public void login(View view){
-
-        String password = passwordInput.getText().toString();
-        String username = usernameInput.getText().toString();
-
-        LoginTask loginTask = new LoginTask(this, new LoginTask.AsyncResponse(){
-            @Override
-            public void processFinish(String output){
-                setToken(output);
-            }
-        });
-        loginTask.execute(username, password, "www.test.se");
-    }
-
-    public void setToken(String token) {
-            this.token = token;
-        Toast toast = Toast.makeText(getApplicationContext(), this.token, Toast.LENGTH_SHORT);
-        toast.show();
     }
 }
