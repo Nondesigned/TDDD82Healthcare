99afd0328a9f3de1900d39e1c8edf53aaebb6bc5
oskgu360 <oskar.isidor.gustafsson@gmail.com>
2017-03-06 16:58:50 +0100

    Pushnotis - Hantering av ta emot request klar

--- app/src/main/java/tddd82/healthcare/MainActivity.java
+++ app/src/main/java/tddd82/healthcare/MainActivity.java
@@ -4,14 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.firebase.iid.FirebaseInstanceId;
-
-import java.io.IOException;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Få tillstånd för internet - ta bort senare
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-
 
         try {
             Intent intent = getIntent();
@@ -35,33 +30,17 @@ public class MainActivity extends AppCompatActivity {
                     String callerid = intent.getStringExtra("CALLER");
                     callingIntent.putExtra("CALLER",callerid);
                     startActivity(callingIntent);
-
                 }
             }
         }
         catch (Exception e){
-
+            e.printStackTrace();
         }
 
-
         Button call = (Button) findViewById(R.id.call);
-        Button button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("bob",FirebaseInstanceId.getInstance().getToken());
-            }
-        });
-
         call.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //TODO Skicka ett användarnamn till servern
-            Javaserver test = new Javaserver();
-                try {
-                    test.sendPost();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                    // TODO skapa tcp anslutning till java samt pushnotisen via HTTP
             }
         });
     }
--- app/src/main/java/tddd82/healthcare/PushNotice.java
+++ app/src/main/java/tddd82/healthcare/PushNotice.java
@@ -1,14 +1,7 @@
 package tddd82.healthcare;
 
-import android.app.Service;
 import android.content.Intent;
-import android.nfc.Tag;
-import android.os.IBinder;
-import android.util.Log;
-import android.widget.Toast;
 
-
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.RemoteMessage;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
