7672ebde331654e214b3d10c34369524ff026bfd
Nondesigned <perkan95@gmail.com>
2017-03-14 07:48:35 +0100

    Fetching pins and puting them on the map. TODO: Update the map upon moving

--- app/src/main/java/tddd82/healthcare/MapsActivity.java
+++ app/src/main/java/tddd82/healthcare/MapsActivity.java
@@ -1,7 +1,12 @@
 package tddd82.healthcare;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +30,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        requestMapPermission();
+
+    }
+
+    private void requestMapPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            mMap.setMyLocationEnabled(true);
+        }else{
+            Log.d("PERMISSION","No permission for location");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
     }
 
 
