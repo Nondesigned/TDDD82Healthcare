ce1740392aadda9ef20f3016fc59969a062f52bf
ottizy <martino_7878@hotmail.com>
2017-03-21 11:23:35 +0100

    Fixing voice/video

--- app/src/main/java/tddd82/healthcare/StartActivity.java
+++ app/src/main/java/tddd82/healthcare/StartActivity.java
@@ -24,8 +24,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-        VideoCall call = new VideoCall();
+        
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
