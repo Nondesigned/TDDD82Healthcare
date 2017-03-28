f10bcbb5e82075c2a9dfcbcc6c5f4951c8478165
ottizy <martino_7878@hotmail.com>
2017-03-28 09:55:31 +0200

    Permission fix

--- app/src/main/java/tddd82/healthcare/StartActivity.java
+++ app/src/main/java/tddd82/healthcare/StartActivity.java
@@ -29,6 +29,7 @@ public class StartActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
 
         requestRecordAudioPermission();
+        requestCameraPermission();
         preferences = context.getSharedPreferences("tddd82.healthcare", context.MODE_PRIVATE);
         editor = preferences.edit();
 
@@ -77,6 +78,26 @@ public class StartActivity extends AppCompatActivity {
                 }
             }
         }
+
+    private void requestCameraPermission(){
+        //check API version, do nothing if API version < 23
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 1);
+            }
+        }
+    }
     public void logout(View view){
         preferences = context.getSharedPreferences("tddd82.healthcare", context.MODE_PRIVATE);
         editor = preferences.edit();
