4c359ad2d7ea903f6cd5b6b9f090ed92db095890
ottizy <martino_7878@hotmail.com>
2017-04-20 12:29:17 +0200

    New branch for graphic design :D

--- app/src/main/java/tddd82/healthcare/ActiveCall.java
+++ app/src/main/java/tddd82/healthcare/ActiveCall.java
@@ -37,7 +37,7 @@ public class ActiveCall extends AppCompatActivity {
                 public void onTimeout(int currentSequenceNumber, int destinationNumber) {
 
                 }
-            }, new CallCrypto(IV, key), (ImageView)findViewById(R.id.imageView2), thisIsIt, (TextView)findViewById(R.id.textView), isVideo);
+            }, new CallCrypto(IV, key), paparazzi, thisIsIt, (TextView)findViewById(R.id.textView), isVideo);
 
             if (callInstance.initialize() != CallError.SUCCESS){
 
@@ -48,7 +48,7 @@ public class ActiveCall extends AppCompatActivity {
         }
     };
 
-
+    private ImageView paparazzi;
     InitCall init;
     int sourceNr;
     int destNr;
@@ -62,7 +62,7 @@ public class ActiveCall extends AppCompatActivity {
         setContentView(R.layout.activity_active_call);
         thisIsIt = this;
         final Button endCall = (Button) findViewById(R.id.endCall);
-        ImageView paparazzi = (ImageView)findViewById(R.id.imageView2);
+        paparazzi = (ImageView)findViewById(R.id.imageView2);
 
         //Get token from SharedPref
         SharedPreferences sharedPreferences = this.getSharedPreferences("tddd82.healthcare", this.MODE_PRIVATE);
